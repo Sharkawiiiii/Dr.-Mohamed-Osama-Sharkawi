29e2996a060beea90f0f02f43f68a0126fa84d0e
Ahmed Yasser El Sharkawy <eg.ahmed.yasser.abdallah@gmail.com>
2026-01-11 20:55:20 +0200

    v1.03

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Dr.-Mohamed-Osama-Sharkawi',
+  images: {
+    unoptimized: true,
+  },
   reactCompiler: true,
 };
 
